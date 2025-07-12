b2015068750ff24895887ce48c9d20c44d6bbc30
AliAlamgirTehrani <didarnour@gmail.com>
2025-07-12 16:02:46 +0330

    Delete yaran_bale_bot_online_final directory

--- yaran_bale_bot_online_final/modules/ocr_passport.py
+++
@@ -1,3 +0,0 @@
-
-def check_passport_validity(image_path):
-    return True  # فرضی: OCR اعتبار را تأیید می‌کند
--- yaran_bale_bot_online_final/modules/validator.py
+++
@@ -1,9 +0,0 @@
-
-def validate_national_code(code):
-    return len(code) == 10 and code.isdigit()
-
-def validate_phone_number(phone):
-    return phone.startswith("09") and len(phone) == 11
-
-def validate_passport_number(passport):
-    return len(passport) >= 8
